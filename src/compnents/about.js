d40fd9103dd23a518bc9a91ef397fcfdd1e747fb
Muhammad Tariq <81058550+iamsheraz@users.noreply.github.com>
2022-05-15 22:23:56 +0500

    Update about.js

--- src/compnents/about.js
+++ src/compnents/about.js
@@ -26,7 +26,7 @@ const About = (props) => {
                                 <a href="mailto:sherazztariq@gmail.com" className="chat">
                                     Let's get in touch!
                                 </a>
-                                <a href="https://docs.google.com/document/d/1zqwhTXhj5UckcOpSX03yGjo5NCRnAKCmWtsy6jtD2Qs/edit?usp=sharing" target="_blank" className="resume">
+                                <a href="https://drive.google.com/file/d/1OT9hoD_b8tRu0E05C599uxGk6SeOthXb/view?usp=drivesdk" target="_blank" className="resume">
                                     PDF Resume
                                 </a>
                             </div>
